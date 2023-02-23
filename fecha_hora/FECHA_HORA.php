c281d480cef16a7e3ff3e59b9e94c27cffd45717
RomelCarlof <108501091+RomelCarlof@users.noreply.github.com>
2023-02-23 14:50:04 -0500

    Add files via upload

---
+++ fecha_hora/FECHA_HORA.php
@@ -0,0 +1,15 @@
+<?php
+
+date_default_timezone_set("America/Lima");
+echo date("d-m-Y h:i:s a");
+?>
+
+<html>
+    <head>
+        <form method = "post" action ="autor.php">
+        Actualice el aplicativo para saber quien es el autor
+            <br>
+            <input type ="submit" value = "Actualizar">
+        </form>
+    </head>
+</html>
---
+++ fecha_hora/autor.php
@@ -0,0 +1,5 @@
+<?php
+$Nombre ="Romel Carlof IZAGUIRRE BARBARAN";
+$line ="</br>";
+echo "El Autor es: ".$Nombre.$line;
+?>
